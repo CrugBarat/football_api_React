e2926ad20b2c377d469eb1e20b86eab1323829d1
CrugBarat <craigtbarratt@gmail.com>
2020-06-16 22:03:52 +0100

    fetches Football API and stores in arr
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/containers/FootballContainer.js
+++ src/containers/FootballContainer.js
@@ -4,6 +4,24 @@ import CompetitionDetails from '../components/CompetitionDetails';
 
 class FootballContainer extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      competitions: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://api.football-data.org/v2/competitions", {
+      headers: {'X-Auth-Token': 'ff3990f4ff41489d9ad1bad2f46eb057'}
+    })
+    .then(res => res.json())
+    .then(competitions => this.setState({competitions: competitions}))
+    .catch(err => console.err);
+  }
+
+
   render() {
     return (
       <Fragment>
