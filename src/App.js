db2d5bff22ee67a7b3782fb1074b8537eb1f8880
CrugBarat <craigtbarratt@gmail.com>
2020-06-16 21:39:43 +0100

    adds FootballContainer component with boilerplate
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import FootballContainer from './containers/FootballContainer'
 
 function App() {
   return (
-
+    <FootballContainer/>
   );
 }
 
