5808e0032fab763e509b05d65b1f3c72678f1cb5
CrugBarat <craigtbarratt@gmail.com>
2020-06-16 22:07:59 +0100

    adds basic returns to components
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/components/CompetitionDetails.js
+++ src/components/CompetitionDetails.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const CompetitionDetails = (prop) => {
 
+  return (
+    <h3>Competition Details</h3>
+  )
 }
 
 export default CompetitionDetails;
--- src/components/CompetitionSelector.js
+++ src/components/CompetitionSelector.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 const CompetitionSelector = (props) => {
 
+  return (
+    <select>
+      <option disabled>Choose a Competition</option>
+    </select>
+  )
 }
 
 export default CompetitionSelector;
